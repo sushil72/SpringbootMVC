8108e93fb6efd91f62093ce73ef016217f05c54d
Coder_Sushil <sushilkumarrohidas0@gmail.com>
2024-05-11 14:52:48 +0530

    MVC using spring boot , SpringbootMVC

---
+++ src/test/java/com/springboot/springbootmvc/SpringbootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
